c1c6cf5cc45a294c61ef8d32f858d3b8b1a7706d
carlostisca <carlose.gonzalez17@gmail.com>
2024-03-08 00:46:51 -0600

    ADDED:JWT CONFIG

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+use App\Http\Controllers\Api\ApiController;
+
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("refresh", [ApiController::class, "refreshToken"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
