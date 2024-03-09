3d112f722aa1b31ab99df17bda450750914f1268
carlostisca <carlose.gonzalez17@gmail.com>
2024-03-09 11:40:22 -0600

    ADDED:LOGING FUNCTION

--- resources/views/Login.vue
+++ resources/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen bg-gray-100 flex justify-center">
-    <div class="py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl">
-      <form action="">
+    <div class="py-6 px-8 h-auto w-auto mt-20 bg-white rounded shadow-xl">
+      <form @submit.prevent="IniciarSesion">
         <div>
           <label for="email" class="block text-gray-800 font-bold"
             >Email:</label
@@ -11,6 +11,7 @@
             name="email"
             id="email"
             placeholder="@email"
+            v-model="email"
             class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
           />
         </div>
@@ -22,6 +23,7 @@
             type="password"
             name="name"
             id="name"
+            v-model="password"
             placeholder="password"
             class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
           />
@@ -31,10 +33,16 @@
             >Forget Password</a
           >
         </div>
-
-        <butt
+          <!-- Mensajes de error y éxito -->
+          <div v-if="showMessage" class="mt-4 p-2 bg-red-100 text-red-600 rounded">
+            {{ errorMessage }}
+          </div>
+          <div v-if="showMessageSuccess" class="mt-4 p-2 bg-green-100 text-green-600 rounded">
+            {{ message }}
+          </div>
+        <button
           class="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded"
-          ><router-link :to="{ name: 'login' }">Login</router-link></butt
+          >Iniciar Sesion</button
         >
       </form>
     </div>
@@ -44,6 +52,33 @@
 <script>
 export default {
   name: "login",
+  data(){
+    return {
+      showMessage: false,
+      showMessageSuccess:false,
+      errorMessage:"",
+      email:"",
+      password:""
+    }
+  },
+  methods:{
+    async IniciarSesion(){
+      try {
+        const response = await this.axios.post("api/login", 
+        {
+          email: this.email,
+          password: this.password
+        },
+        )
+        this.showMessageSuccess = true;
+        this.message = "Welcome!";
+      } catch (error) {
+        const errorMessage = error.response.data?.message || "Ha ocurrido un error";
+          this.errorMessage = errorMessage;
+          this.showMessage = true;
+      }
+    }
+  }
 };
 </script>
 
--- resources/views/Register.vue
+++ resources/views/Register.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-screen bg-gray-100 flex justify-center">
-      <div class="py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl">
-        <form @submit.prevent="iniciarSesion">
+      <div class="py-6 px-8 h-auto w-auto mt-20 bg-white rounded shadow-xl">
+        <form @submit.prevent="createUser">
           <div>
             <label for="name" class="block text-gray-800 font-bold">Nombre:</label>
             <input type="text" name="name" id="name" placeholder="Andrea Sanchez" v-model="name" class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
@@ -10,8 +10,10 @@
             <label for="email" class="block text-gray-800 font-bold">Email:</label>
             <input type="text" name="email" id="email" placeholder="@email" v-model="email" class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
           </div>
-          <div class="mb-6">
+          <div>
             <label for="candy" class="block text-gray-800 font-bold">Password:</label>
+          </div>
+          <div>
             <input type="password" name="candy" v-model="password" id="password" placeholder="password" class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
             <label for="candyconfirm" class="block text-gray-800 font-bold">Confirmar password:</label>
             <input type="password" name="candyconfirm" v-model="password_confirmation" id="confirm" placeholder="confirm password" class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
@@ -29,7 +31,7 @@
           <!-- Botón de registro -->
           <button class="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">Registrarse</button>
         </form>
-      </div>
+    </div>
     </div>
   </template>
   
@@ -49,7 +51,7 @@
       };
     },
     methods: {
-      async iniciarSesion() {
+      async createUser() {
         try {
           const response = await this.axios.post("api/register", {
             email: this.email,
