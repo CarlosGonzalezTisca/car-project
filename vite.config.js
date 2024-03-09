f4c641bfa9c5388581cac5fb713aead2d20fabbd
carlostisca <carlose.gonzalez17@gmail.com>
2024-03-08 23:29:17 -0600

    CONFIG: VUEJS PORTS SERVERING

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
+    vue(),
+  ],
+  server: {
+    port: parseInt(process.env.VUE_LOCAL_PORT),
+    host: '0.0.0.0'
+  },
 });
